420efdcedeb80fab1d10236e181c1952fc66d636
Sam Ehlers <groveriffic@gmail.com>
2016-04-11 11:17:05 -0500

    Removed individual field comments
    Looked cluttered in Go Doc

--- handwritingio/handwritingio.go
+++ handwritingio/handwritingio.go
@@ -1,4 +1,6 @@
 // Package handwritingio provides a client library for interacting with the handwriting.io API
+//
+// Additional API documentation available at https://handwriting.io/docs/
 package handwritingio
 
 import (
@@ -15,31 +17,20 @@ import (
 
 // Handwriting contains information about a handwriting style
 type Handwriting struct {
-	// ID is an unique identifier for the handwriting style
-	ID string `json:"id"`
-	// Title is human friendly name for the handwriting
-	Title string `json:"title"`
-	// Created is when the handwriting was created
-	Created time.Time `json:"date_created"`
-	// Modified is when then handwriting was last modified
-	Modified time.Time `json:"date_modified"`
-	// RatingNeatness is a rating of how "neat" a handwriting is relative to others
-	RatingNeatness int `json:"rating_neatness"`
-	// RatingEmbellishment is a rating of how "embellished" a handwriting is relative to others
-	RatingEmbellishment int `json:"rating_embellishment"`
-	// RatingCharacterWidth is a rating of how wide a handwriting is relative to others
-	RatingCharacterWidth int `json:"rating_character_width"`
+	ID                   string    `json:"id"`
+	Title                string    `json:"title"`
+	Created              time.Time `json:"date_created"`
+	Modified             time.Time `json:"date_modified"`
+	RatingNeatness       int       `json:"rating_neatness"`
+	RatingEmbellishment  int       `json:"rating_embellishment"`
+	RatingCharacterWidth int       `json:"rating_character_width"`
 }
 
 // HandwritingListParams contains the parameters for listing handwritings
 type HandwritingListParams struct {
-	// Offset is the number of handwritings to skip at the beginning of the list.  Useful for pagination.
-	Offset int
-	// Limit is the maximum number of handwritings to return.  Useful for pagination
-	Limit int
-	// OrderBy is the name of the field to use for sorting
-	OrderBy string
-	// OrderDir is the direction to sort.  Use "asc" for ascending, "desc" for descending.
+	Offset   int
+	Limit    int
+	OrderBy  string
 	OrderDir string
 }
 
